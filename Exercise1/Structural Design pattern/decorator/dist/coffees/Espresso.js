0d8f869ffcb60d696a26aaeceb5a18c80228547e
HariniiSubramanian <hariniisubramanian@gmail.com>
2024-07-27 23:09:35 +0530

    Add files via upload

---
+++ Exercise1/Structural Design pattern/decorator/dist/coffees/Espresso.js
@@ -0,0 +1,13 @@
+"use strict";
+// src/coffees/Espresso.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Espresso = void 0;
+class Espresso {
+    getDescription() {
+        return "Espresso";
+    }
+    cost() {
+        return 1.99;
+    }
+}
+exports.Espresso = Espresso;
---
+++ Exercise1/Structural Design pattern/decorator/dist/coffees/HouseBlend.js
@@ -0,0 +1,13 @@
+"use strict";
+// src/coffees/HouseBlend.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HouseBlend = void 0;
+class HouseBlend {
+    getDescription() {
+        return "House Blend Coffee";
+    }
+    cost() {
+        return 0.89;
+    }
+}
+exports.HouseBlend = HouseBlend;
